8d3707e0b59784ac6f7165545e9832e52b23580d
dangobassline <dangobassline@gmail.com>
2020-10-26 13:38:58 -0300

    docs agregado

---
+++ docs/precache-manifest.f7b8ac57500d5f78d25a6b0d32859400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b35a954efc9123750d049151c0fc2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40bb3a0ffcaf812051e8",
+    "url": "/static/css/main.26cd2665.chunk.css"
+  },
+  {
+    "revision": "717723dea590047e429d",
+    "url": "/static/js/2.f9c300ca.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.f9c300ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bb3a0ffcaf812051e8",
+    "url": "/static/js/main.26f5f7f9.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
